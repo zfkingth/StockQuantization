3922cc67694e8b19809700f4eccd34e4b4aa3b20
zfking <zfking@gmail.com>
2020-03-12 11:47:31 +0800

    使用lazy ，优化http client

--- Stock.JQData/QueryFun.cs
+++ Stock.JQData/QueryFun.cs
@@ -2,6 +2,7 @@
 using Stock.Model;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -13,74 +14,95 @@ namespace Stock.JQData
     {
         public static string _token;
 
+        public static HttpClient SingleClient { get { return lazy.Value; } }
+
+        private static readonly Lazy<HttpClient> lazy =
+    new Lazy<HttpClient>(
+        () =>
+        {
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+                                  | DecompressionMethods.Deflate
+
+
+            };
+            var client = new HttpClient(handler);
+
+
+
+            client.BaseAddress = new Uri("https://dataapi.joinquant.com/apis");
+
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("KeepAlive", "true");
+            client.DefaultRequestHeaders.ExpectContinue = true;
+
+
+            return client;
+        }
+        );
+
+
         /// <summary>
         /// 获取用户token，并保存到类的静态字段中。
         /// </summary>
         /// <returns></returns>
         public static string Get_token()
         {
-            using (var client = new HttpClient())
+
+            object body = new
             {
-
-
-                object body = new
-                {
-                    method = "get_token",
-                    mob = "18080802572", //mob是申请JQData时所填写的手机号
-                    pwd = "dragon00A" //Password为聚宽官网登录密码，新申请用户默认为手机号后6位
-                };
+                method = "get_token",
+                mob = "18080802572", //mob是申请JQData时所填写的手机号
+                pwd = "dragon00A" //Password为聚宽官网登录密码，新申请用户默认为手机号后6位
+            };
 
 
 
 
-                //读取返回的TOKEN
-                _token = QueryInfo(client, body);
+            //读取返回的TOKEN
+            _token = QueryInfo(body);
 
-                return _token;
+            return _token;
 
-            }
+
 
         }
 
         public static string Get_all_securities()
         {
-            using (var client = new HttpClient())
+            //查询所有股票代码
+            var body = new
             {
+                method = "get_all_securities",
+                token = _token, //token
+                code = "stock",
+                date = DateTime.Now.ToString(PubConstan.DateFormatString)
+            };
+            string info = QueryInfo(body);
 
-                //查询所有股票代码
-                var body = new
-                {
-                    method = "get_all_securities",
-                    token = _token, //token
-                    code = "stock",
-                    date = DateTime.Now.ToString(PubConstan.DateFormatString)
-                };
-                string info = QueryInfo(client, body);
+            return info;
 
-                return info;
-            }
 
         }
 
-        public static string Get_price(StockContext db, Securities sec)
+        public static string Get_price(StockContext db, Securities sec, int cnt)
         {
-            using (var client = new HttpClient())
+
+            var body = new
             {
+                method = "get_price",
+                token = _token,
+                code = sec.Code,
+                count = cnt,
+                unit = "1d",
+                end_date = DateTime.Now.ToString(PubConstan.DateFormatString)
+            };
+            string info = QueryInfo(body);
 
+            return info;
 
-                var body = new
-                {
-                    method = "get_price",
-                    token = _token,
-                    code = sec.Code,
-                    count = 100,
-                    unit = "1d",
-                    end_date = DateTime.Now.ToString(PubConstan.DateFormatString)
-                };
-                string info = QueryInfo(client, body);
-
-                return info;
-            }
         }
 
         public static string Get_security_info()
@@ -91,10 +113,9 @@ namespace Stock.JQData
 
 
 
-        protected static string QueryInfo(HttpClient client, object body)
+        protected static string QueryInfo(object body)
         {
-
-            const string url = "https://dataapi.joinquant.com/apis";
+            var client = SingleClient;
             var options = new JsonSerializerOptions
             {
                 WriteIndented = true
@@ -106,7 +127,7 @@ namespace Stock.JQData
             StringContent bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
 
             //POST请求并等待结果
-            var result = client.PostAsync(url, bodyContent).Result;
+            var result = client.PostAsync("", bodyContent).Result;
 
 
             return result.Content.ReadAsStringAsync().Result;
