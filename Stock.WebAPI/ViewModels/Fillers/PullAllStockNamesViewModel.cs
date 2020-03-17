17385fe7aebf0e6b4f0321d241ee07b9271f5146
zfking <zfking@gmail.com>
2020-03-17 20:51:48 +0800

    能够正确设置更新标记

--- Stock.WebAPI/ViewModels/Fillers/PullAllStockNamesViewModel.cs
+++ Stock.WebAPI/ViewModels/Fillers/PullAllStockNamesViewModel.cs
@@ -1,19 +1,70 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Stock.Data;
+using Stock.JQData;
+using Stock.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Stock.WebAPI.ViewModels.Fillers
 {
-    public class PullAllStockNamesViewModel
+    public class PullAllStockNamesViewModel : BaseDoWorkViewModel
     {
-        internal Task PullAll()
+        private readonly ILogger _logger;
+        public PullAllStockNamesViewModel(IServiceScopeFactory serviceScopeFactory,
+
+            ILogger<PullAllStockNamesViewModel> logger) : base(serviceScopeFactory)
         {
+            _logger = logger;
+        }
+
+
+
+        internal async Task PullAll()
+        {
+            await setStartDate();
+
             JQData.HandleFun hf = new JQData.HandleFun();
 
             var res = hf.Update_allStock_Names();
-            return res;
+
+            await setFinishedDate();
 
         }
+
+        private async Task setStartDate()
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<StockContext>();
+
+                var record = db.StockEvents.First(s => s.EventName == Constants.EventPullStockNames);
+
+                record.LastAriseStartDate = DateTime.Now;
+                record.Status = EventStatusEnum.Running;
+                await db.SaveChangesAsync();
+
+            }
+        }
+
+        private async Task setFinishedDate()
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<StockContext>();
+
+                var record = db.StockEvents.First(s => s.EventName == Constants.EventPullStockNames);
+
+                record.LastAriseEndDate = DateTime.Now;
+                record.Status = EventStatusEnum.Idle;
+                await db.SaveChangesAsync();
+
+            }
+        }
+
     }
 }
