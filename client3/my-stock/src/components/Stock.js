cc7d1de5310b9029a073e017776cd7028cca77f2
zfking <zfking@gmail.com>
2020-03-29 10:39:02 +0800

    简单的用对数处理

--- client3/my-stock/src/components/Stock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import HighchartsReact from 'highcharts-react-official'
-
-const StockChart = ({ options, highcharts }) => <HighchartsReact
-  highcharts={highcharts}
-  constructorType={'stockChart'}
-  options={options}
-/>
-export default StockChart
