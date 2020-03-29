54ae65f6469c30c3ee21524481380a8dede7cb82
zfking <zfking@gmail.com>
2020-03-29 12:16:30 +0800

    hello world 成功

--- client3/my-stock/src/App.js
+++ client3/my-stock/src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Highcharts from 'highcharts/highstock';
-import HighchartsReact from 'highcharts-react-official';
 
-const options = {
-  yAxis: {
-    type: 'logarithmic',
-  },
-  title: {
-    text: 'My stock chart'
-  },
-  series: [
-    {
-      data: [1, 2, 1, 4, 3, 6,10,20,30,60,100,150,300]
-    }
-  ]
-};
+import HighStockDemo from './HighStockDemo'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <HighchartsReact
-      highcharts={Highcharts}
-      constructorType={'stockChart'}
-      options={options}
-    />
+      <HighStockDemo />
     </div>
   );
 }
---
+++ client3/my-stock/src/HighStockDemo.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import stockData from './data';
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+
+class HighStockDemo extends React.Component {
+    render() {
+      return (
+       <h1>Hello World!</h1>
+      );
+    }
+  }
+export default HighStockDemo;
+  
