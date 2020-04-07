e8122f640e8ac11b807d706ad2654ccff8924e9c
zfking <zfking@gmail.com>
2020-04-07 16:34:44 +0800

    能够很好的处理时间的格式

--- client3/my-stock/src/components/MyIndex/index.js
+++ client3/my-stock/src/components/MyIndex/index.js
@@ -34,6 +34,17 @@ color:red;
 
 const shicha = 8 * 3600 * 1000;
 
+function timeStamp2String(time) {
+  var datetime = new Date();
+  datetime.setTime(time);
+  var year = datetime.getFullYear();
+  var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+  var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+  // var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();  
+  // var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();  
+  // var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();  
+  return year + "-" + month + "-" + date;  //+" "+hour+":"+minute+":"+second;  
+}
 
 function handleMarginData(marginArray) {
   let myMap = new Map();
@@ -182,7 +193,7 @@ const createOption = (stockInfo, historyData, marginData) => {
           const rt = s + '<br/>' + con;
 
           return rt;
-        }, '<b>' + this.x + '</b>');
+        }, '<b>' + timeStamp2String(this.x) + '</b>');
       },
     },
 
