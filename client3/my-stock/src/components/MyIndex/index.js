4005a01fd1f3ace2844e84fadd6f5576d1c02da3
zfking <zfking@gmail.com>
2020-04-07 16:41:21 +0800

    使用const

--- client3/my-stock/src/components/MyIndex/index.js
+++ client3/my-stock/src/components/MyIndex/index.js
@@ -35,11 +35,11 @@ color:red;
 const shicha = 8 * 3600 * 1000;
 
 function timeStamp2String(time) {
-  var datetime = new Date();
+  const datetime = new Date();
   datetime.setTime(time);
-  var year = datetime.getFullYear();
-  var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
-  var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+  const year = datetime.getFullYear();
+  const month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+  const date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
   // var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();  
   // var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();  
   // var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();  
