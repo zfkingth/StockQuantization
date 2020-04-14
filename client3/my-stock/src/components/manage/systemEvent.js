683b423675fdcc16982791f8e36f6abf10ec4b11
zfking <zfking@gmail.com>
2020-04-14 11:41:28 +0800

    能够定位事件

--- client3/my-stock/src/components/manage/systemEvent.js
+++ client3/my-stock/src/components/manage/systemEvent.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import DataGrid, { Column, Editing } from 'devextreme-react/data-grid';
+import Button from 'devextreme-react/button';
 
 
 import { connectTo } from '../../utils/generic';
 
 class DemoBase extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = { events: [] };
+  
+
+  }
+
+  logEvent=(eventName) =>{
+    this.setState((state) => {
+      return { events: [eventName].concat(state.events) };
+    });
+  }
+
+  clearEvents=() =>{
+    this.setState({ events: [] });
+  }
 
   isChief = (position) => {
     return position && ['CEO', 'CMO'].indexOf(position.trim().toUpperCase()) >= 0;
@@ -34,12 +52,15 @@ class DemoBase extends React.PureComponent {
 
     // employees.splice(e.row.rowIndex, 0, clonedItem);
     // this.setState({ employees: employees });
+    const eventName=e.row.data.eventName;
+    this.logEvent(eventName);
     e.event.preventDefault();
   }
   render() {
 
     const { rows, } = this.props;
     return (
+      <React.Fragment>
       <DataGrid
         id="gridContainer"
         dataSource={rows}
@@ -50,13 +71,12 @@ class DemoBase extends React.PureComponent {
         <Editing
           mode="row"
           useIcons={true}
-          allowUpdating={true}
-          allowDeleting={this.allowDeleting} />
+         />
         <Column type="buttons" width={110}
-          buttons={['edit', 'delete', {
-            hint: 'Clone',
+          buttons={[ {
+            hint: 'Arise',
             icon: 'repeat',
-            visible: this.isCloneIconVisible,
+         
             onClick: this.cloneIconClick
           }]} />
         <Column dataField="eventName" caption="Title" />
@@ -65,6 +85,17 @@ class DemoBase extends React.PureComponent {
         <Column dataField="status" />
 
       </DataGrid>
+            <div id="events">
+            <div>
+  
+              <div className="caption">Fired events</div>
+              <Button id="clear" text="Clear" onClick={this.clearEvents} />
+            </div>
+            <ul>
+              {this.state.events.map((event, index) => <li key={index}>{event}</li>)}
+            </ul>
+          </div>
+          </React.Fragment>
     );
   }
 }
