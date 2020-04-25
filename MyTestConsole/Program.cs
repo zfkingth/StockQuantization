f29e3f09d429637337ced532f4c1c40174184570
zfking <zfking@gmail.com>
2020-04-25 10:44:45 +0800

    能使用headless 浏览器获取数据

--- MyTestConsole/Program.cs
+++ MyTestConsole/Program.cs
@@ -7,14 +7,30 @@ namespace MyTestConsole
 {
     class Program
     {
+        public static object TimeUnit { get; private set; }
+
         static void Main(string[] args)
         {
+            ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;//关闭黑色cmd窗口
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--disable-gpu");
+            //禁用图片
+            options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+            options.AddArgument("--headless");
+
+
             Console.Write("test case started ");
             //create the reference for the browser  
-            IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver(driverService, options);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+
+
+
             // navigate to URL  
             driver.Navigate().GoToUrl("http://data.eastmoney.com/hsgt/index.html");
-            Thread.Sleep(2000);
+            //Thread.Sleep(2000);
             //// identify the Google search text box  
             //IWebElement ele = driver.FindElement(By.Name("q"));
             ////enter the value in the google search text box  
@@ -24,10 +40,10 @@ namespace MyTestConsole
             //IWebElement ele1 = driver.FindElement(By.Name("btnK"));
             //// click on the Google search button  
             //ele1.Click();
-            Thread.Sleep(3000);
+            //Thread.Sleep(3000);
 
-            IWebElement ele = driver.FindElement(By.Id("zjlx_hgt"));
-            ele = driver.FindElement(By.XPath("//*[@id=\"zjlx_hgt\"]/td[5]/span"));
+
+            var ele = driver.FindElement(By.XPath("//*[@id=\"zjlx_hgt\"]/td[5]/span"));
             var str = ele.Text;
 
             //close the browser  
